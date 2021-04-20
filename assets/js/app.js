33e23d43e0059b343913443c860aa48a6f82720b
Paul Abakada <33938655+paulynson@users.noreply.github.com>
2021-04-20 17:27:29 +0100

    Number fact

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,63 +1,26 @@
-const input = document.querySelector(".inputValue");
-const search = document.querySelector(".btn-search");
-const result = document.querySelector(".message-box");
+let inputText = document.querySelector(".inputValue");
+let search = document.querySelector(".btn-search");
+let result = document.querySelector(".message-box");
+let numText = document.querySelector(".num");
+let msg = document.querySelector(".message-box");
 
-search.addEventListener("click", function(){
-    // const getMap = async () =>{
-    //     const response = await fetch('https://www.googleapis.com/customsearch/v1?key=AIzaSyBT0k8at-27YAAkSjQ8TxGO1rsb3okjweA&cx=017576662512468239146:omuauf_lfve&q=lectures');
-    //     if (response.status !== 200){
-    //         throw new Error("data cannot be fetched")
-    //     }
-    
-    //     const data = await response.json();
-    //       console.log(data)
+inputText.addEventListener("input", function(){
+  let number = inputText.value;
 
-    // }
-    // getMap()
-    // .then(data => { 
-    //     var namePair = data["queries"];
-    //     result.innerHTML = namePair;
-    //     console.log(data)
-    // })
-    // .catch(err => console.log("reject", err.message))
-
-   
-    fetch("https://www.googleapis.com/customsearch/v1?key=AIzaSyBT0k8at-27YAAkSjQ8TxGO1rsb3okjweA&cx=017576662512468239146:omuauf_lfve&q='+input.value+'lectures'")
-    .then(function(text) {
-        return text()
-        .then(function(text) {
-       var namePair = text['queries'];
-        result.innerHTML = namePair;
-          console.log("resolve", text)
-      
-        })
-        .catch(err => console.log("reject", err.message))
-      });
-
-
-
-
-
-
-
-
-//    fetch('https://www.googleapis.com/customsearch/v1?key=AIzaSyBT0k8at-27YAAkSjQ8TxGO1rsb3okjweA&cx=017576662512468239146:omuauf_lfve&q='+input.value+'lectures')
-
-   
-// .then(response => response.json())
-// .then(data => {
-// var namePair = data['queries'];
-
-//  result.innerHTML = namePair;
-
-//   console.log(data)
-
-// })
-
-
-// .catch(err => console.log("reject", err.message))
+  fetch('http://numbersapi.com/'+number)
+    .then(response => response.text())
+    .then(data => {
+      if(data.status = 200 && number !== ''){
+        msg.style.display = "block";
+        numText.innerText = data;
+      }        
+     })
+      .catch(err => console.log(err.message));
 })
 
+  
 
 
-// })
+    
+
+  

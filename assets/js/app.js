3daa917bb845ed5e8be181af3e82b8ea7a2dd0d2
Paul Abakada <33938655+paulynson@users.noreply.github.com>
2022-04-02 12:02:41 +0100

    fix fetch with async method and adding comment to the js script

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,12 +1,22 @@
+// Declare values
+
 let inputText = document.querySelector(".inputValue");
 let search = document.querySelector(".btn-search");
 let result = document.querySelector(".message-box");
 let numText = document.querySelector(".num");
 let msg = document.querySelector(".message-box");
 
+// Add event listener to the input element
+
 inputText.addEventListener("input", () => {
 
+
+  // Fetching data with async and a function that does the magic
+
   const fetchData = async () => {
+
+// Declaring input values and the url for the endpoint
+
     let number = inputText.value;
     let url = "http://numbersapi.com/";
   
